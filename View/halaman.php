62cc4c4c1483d873f3e26f59723fa5d4c4bf28c6
BagasHtml <ida.indawati@gmail.com>
2025-12-07 00:08:00 +0700

    update dikit

---
+++ View/halaman.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Clausewitz - Catalyst</title>
+    <link rel="stylesheet" href="../Design/style.css">
+</head>
+<body>
+    <!-- Navbar -->
+    <div class="container">
+        <div class="container-nav">
+            <h1>Selamat Datang di</h1>
+            <h1 style="color: gray;">Knowledge bat<span style="color: red;">tle information</span></h1>
+        </div>
+    </div>
+
+    <!-- Main -->
+    <main>
+        <div class="main-container">
+            <div class="main">
+                <h1>Tentang Kami</h1>
+                <p>Siapa Kami?</p>
+                <p>
+                
+                </p>
+            </div>
+        </div>
+    </main>
+
+    <!-- footer -->
+    <footer>
+        <div class="footer-container">
+            <div class="footer">
+
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
